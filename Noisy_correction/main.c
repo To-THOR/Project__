20b4c53ff4d20f40725049b070b524d79ad83e16
To-THOR <victor.piton@epfl.ch>
2020-04-14 16:10:42 +0200

    Rotation on itself

--- Noisy_correction/main.c
+++ Noisy_correction/main.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <chprintf.h>
+#include <motors.h>
+//#include <audio/microphone.h>
 
+#include <audio_processing.h>
+#include <fft.h>
+#include <communications.h>
+#include <arm_math.h>
+
+#include "memory_protection.h"
 
 // moteur 360°
 
@@ -15,15 +29,25 @@
 
 int main(void)
 {
+	halInit();
+	chSysInit();
+	mpu_init();
+
+	motors_init();
+
+
 	e_init_port();
 	e_init_motors();
+
+	left_motor_set_speed(500);
+	right_motor_set_speed(-500);
 	//{This function manage the left motor speed by changing the MOTOR1 phases.
 	//The changing phases frequency (=> speed) is controled by the agenda
 	//param motor_speed from -1000 to 1000 give the motor speed in steps/s,
 	//positive value to go forward and negative to go backward.}
-	e_set_speed(-500, 500);
+	////////e_set_speed(-500, 500);
 	//e_set_speed(int linear_speed, int angular_speed)
-	e_start_agendas_processing();
+	///////e_start_agendas_processing();
 	// { reset Timer2 CONtrol register
 	// precsaler = 1
 	// clear timer 2
@@ -31,6 +55,13 @@ int main(void)
 	// clear interrupt flag
 	// set interrupt enable bit
 	// start Timer2 }
-while(1) {}
+while(1) {;}
 }
 
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
