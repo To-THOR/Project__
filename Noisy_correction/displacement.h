028222938d3b60cae39ebe88c7073642f86efb95
To-THOR <victor.piton@epfl.ch>
2020-04-24 17:33:27 +0200

    displacement update (circle drawing first implementation)

--- Noisy_correction/displacement.h
+++ Noisy_correction/displacement.h
@@ -1,10 +1,11 @@
 #ifndef DISPLACEMENT_H_
 #define DISPLACEMENT_H_
 
+
 // --------------------------- FUNCTIONS ---------------------------
 
 void 		displacement_start(void);
 uint16_t 	displacement_distance_get(void);
-void displacement_distance_reset(void);
+void 		displacement_distance_reset(void);
 
 #endif /* DISPLACEMENT_H_ */
