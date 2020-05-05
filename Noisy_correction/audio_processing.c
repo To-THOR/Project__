8c7b9c5328e110b060fa386fbec47345d7193188
To-THOR <victor.piton@epfl.ch>
2020-05-05 09:20:17 +0200

    Cleaned audio-processing

--- Noisy_correction/audio_processing.c
+++ Noisy_correction/audio_processing.c
@@ -36,15 +36,9 @@
 // --------------------------- VARIABLES ---------------------------
 
 //2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
-static float micLeft_cmplx_input[2 * FFT_SIZE];
-static float micRight_cmplx_input[2 * FFT_SIZE];
 static float micFront_cmplx_input[2 * FFT_SIZE];
-static float micBack_cmplx_input[2 * FFT_SIZE];
 //Arrays containing the computed magnitude of the complex numbers
-static float micLeft_output[FFT_SIZE];
-static float micRight_output[FFT_SIZE];
 static float micFront_output[FFT_SIZE];
-static float micBack_output[FFT_SIZE];
 
 /*
  * Name: 		-max_norm_table
@@ -169,40 +163,6 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 
 			sound_remote(micFront_output);
 		}
-	}
-}
-
-/*void wait_send_to_computer(void){
-	chBSemWait(&sendToComputer_sem);
-} */
-
-float* get_audio_buffer_ptr(BUFFER_NAME_t name){
-	if(name == LEFT_CMPLX_INPUT){
-		return micLeft_cmplx_input;
-	}
-	else if (name == RIGHT_CMPLX_INPUT){
-		return micRight_cmplx_input;
-	}
-	else if (name == FRONT_CMPLX_INPUT){
-		return micFront_cmplx_input;
-	}
-	else if (name == BACK_CMPLX_INPUT){
-		return micBack_cmplx_input;
-	}
-	else if (name == LEFT_OUTPUT){
-		return micLeft_output;
-	}
-	else if (name == RIGHT_OUTPUT){
-		return micRight_output;
-	}
-	else if (name == FRONT_OUTPUT){
-		return micFront_output;
-	}
-	else if (name == BACK_OUTPUT){
-		return micBack_output;
-	}
-	else{
-		return NULL;
 	}
 }
 
--- Noisy_correction/audio_processing.h
+++ Noisy_correction/audio_processing.h
@@ -25,16 +25,6 @@ typedef enum {
 void processAudioData(int16_t *data, uint16_t num_samples);
 
 /*
-*	put the invoking thread into sleep until it can process the audio datas
-*/
-//void wait_send_to_computer(void);
-
-/*
-*	Returns the pointer to the BUFFER_NAME_t buffer asked
-*/
-float* get_audio_buffer_ptr(BUFFER_NAME_t name);
-
-/*
  * Name: 		-max_norm_buff_reset
  * Description:	-resets the values of max_norm_table
  * Arguments:	-void
